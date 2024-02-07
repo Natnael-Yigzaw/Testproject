02ba10199ecf29960bf6013e241bd9b2dfd7e815
tihitina <tihitinadesta16@gmail.com>
2024-02-07 05:19:04 -0800

    Add login screen

--- Admin/loginform/login.jsx
+++ Admin/loginform/login.jsx
@@ -1,23 +1,43 @@
-import React from 'react';
-import './login.css'; 
-const login = () => {
-    return (
-        <div div className='wrapper'>
-            <form action=''>
-                <h1>Login</h1>
-                <div className='input-box'>
-                    <input type='text' placeholder='username' required/>
-                
-                </div>
-                <div className='input-box'>
-                    <input type='password' placeholder='password' required/>
-                
-                </div>
-                
-                            </form>
-            
-            
-            </div>
-    )
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // You can implement your login logic here
+    console.log('Logging in with:', { username, password });
+  };
+
+  return (
+    <div className="App">
+      <div className="login-container">
+        <h2>Login</h2>
+        <form>
+          <label>
+            Username:
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <button type="button" onClick={handleLogin}>
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
-export default login;
+
+export default App;
