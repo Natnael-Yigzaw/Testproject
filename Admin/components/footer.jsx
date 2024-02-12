72a35753fc2581651875146b6610495c09074b30
tihitina <tihitinadesta16@gmail.com>
2024-02-12 04:37:14 -0800

    added footer

---
+++ Admin/components/footer.jsx
@@ -0,0 +1,13 @@
+// Footer.js
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2024 Your Website Name. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
