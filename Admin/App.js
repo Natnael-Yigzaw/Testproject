74a738f6f6ee95937c3cb41c994ae88eb6e908ec
tihitina <tihitinadesta16@gmail.com>
2024-02-12 05:35:45 -0800

    update login.jsx and added app.js

---
+++ Admin/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './components/login_form/login';
+
+function App() {
+  return (
+    <div >
+      <Login/> 
+      
+    </div>
+  );
+};
+
+export default App;
--- Admin/pages/login.jsx
+++ Admin/pages/login.jsx
@@ -1,43 +1,24 @@
-import React, { useState } from 'react';
-import './App.css';
+import React from "react"; 
 
-function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleLogin = () => {
-    // You can implement your login logic here
-    console.log('Logging in with:', { username, password });
-  };
-
-  return (
-    <div className="App">
-      <div className="login-container">
-        <h2>Login</h2>
-        <form>
-          <label>
-            Username:
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </label>
-          <label>
-            Password:
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </label>
-          <button type="button" onClick={handleLogin}>
-            Login
-          </button>
-        </form>
-      </div>
-    </div>
-  );
-}
-
-export default App;
+const login = () => {
+    return(
+        
+        <div className="wrapper">
+            <form action="">
+                <h1>Login</h1>
+            <div className="input_box">
+        <input type ="number" placeholder="Enter phone" required /> 
+        </div>
+        <div className="input_box">
+        <input type ="password" placeholder="password" required /> 
+        </div>
+        
+        <button type="submit">Login </button>
+        
+            </form>
+            </div>
+       
+    
+    );
+};
+export default login;
