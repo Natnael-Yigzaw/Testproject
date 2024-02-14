61cacfb67b6e107ee99182f218b0d2aad0597216
tihitina <tihitinadesta16@gmail.com>
2024-02-14 13:38:34 -0800

    added dashboard

---
+++ Admin/pages/DashboardPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>Admin Dashboard - Overview</h1>
+      {/* Dashboard content goes here */}
+    </div>
+  );
+};
+
+export default DashboardPage;
