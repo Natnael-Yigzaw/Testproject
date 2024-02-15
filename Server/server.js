255b5cfaf1af3875564ba72ff20a0df34c557187
natnaely <natnaelky2020@gmail.com>
2024-02-15 18:42:38 +0000

    refactoring the backend

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const connectDb = require("./config/db");
 const userRoute = require("./routes/UserRoute");
+const PORT = process.env.PORT || 4500;
 
 connectDb();
 const app = express();
 
 app.use("/users", userRoute);
 
-app.listen(3000, () => console.log("Server started on port 3000"));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
